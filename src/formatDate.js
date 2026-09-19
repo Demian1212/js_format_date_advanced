d0776170312cd06dc83e7f8a9be52d058932a666
Demian Pishchak <demanpisak191@gmail.com>
2026-09-19 22:33:23 +0300

    solve format date advanced task

--- src/formatDate.js
+++ src/formatDate.js
@@ -4,11 +4,40 @@
  * @param {string} date
  * @param {string[]} fromFormat
  * @param {string[]} toFormat
- *
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const parts = date.split(/[/.-]/);
+  const dateMap = {};
+
+  // Розділювач для результату
+  const targetDelimiter = toFormat[toFormat.length - 1];
+
+  // Заповнюємо dateMap за вхідним форматом
+  for (let i = 0; i < fromFormat.length - 1; i++) {
+    dateMap[fromFormat[i]] = parts[i];
+  }
+
+  // Конвертація YYYY та YY
+  if (dateMap['YYYY'] && !dateMap['YY']) {
+    dateMap['YY'] = dateMap['YYYY'].slice(-2);
+  } else if (dateMap['YY'] && !dateMap['YYYY']) {
+    const yearNum = Number(dateMap['YY']);
+    const century = yearNum < 30 ? '20' : '19';
+
+    dateMap['YYYY'] = century + dateMap['YY'];
+  }
+
+  // Збираємо результат
+  const result = [];
+
+  for (let i = 0; i < toFormat.length - 1; i++) {
+    const key = toFormat[i];
+
+    result.push(dateMap[key]);
+  }
+
+  return result.join(targetDelimiter);
 }
 
 module.exports = formatDate;
